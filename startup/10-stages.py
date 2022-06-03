96a2988eb6d895dee8cbb137a1cf7d7dc6a8b72e
Thomas A Caswell <tcaswell@bnl.gov>
2022-06-03 11:44:23 -0400

    MNT: remove non-maia devices and plans

--- startup/10-stages.py
+++ startup/10-stages.py
@@ -1,14 +1,5 @@
 from ophyd import EpicsMotor, Device, Component as Cpt
 
-class Stage(Device):
-    x    = Cpt(EpicsMotor, '{UTS:1-Ax:X}Mtr')
-    y    = Cpt(EpicsMotor, '{UTS:1-Ax:Y}Mtr')
-    z    = Cpt(EpicsMotor, '{UTS:1-Ax:Z}Mtr')
-
-S = Stage('XF:04BMC-ES:2', name='S')
-S_x   = S.x
-S_y   = S.y
-S_z   = S.z
 
 class MaiaStage(Device):
     x    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaX}Mtr')
