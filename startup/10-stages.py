9a5e5dac3b366bb138772af1f95cf07c78609489
Thomas A Caswell <tcaswell@bnl.gov>
2022-06-03 11:46:38 -0400

    STY: run black on remaining files

--- startup/10-stages.py
+++ startup/10-stages.py
@@ -2,13 +2,14 @@ from ophyd import EpicsMotor, Device, Component as Cpt
 
 
 class MaiaStage(Device):
-    x    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaX}Mtr')
-    y    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaY}Mtr')
-    z    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaZ}Mtr')
-    r    = Cpt(EpicsMotor, '{SR50pp:1-Ax:MaiaR}Mtr')
+    x = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaX}Mtr")
+    y = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaY}Mtr")
+    z = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaZ}Mtr")
+    r = Cpt(EpicsMotor, "{SR50pp:1-Ax:MaiaR}Mtr")
 
-M = MaiaStage('XF:04BMC-ES:2', name='M')
-M_x   = M.x
-M_y   = M.y
-M_z   = M.z
-M_r   = M.r
+
+M = MaiaStage("XF:04BMC-ES:2", name="M")
+M_x = M.x
+M_y = M.y
+M_z = M.z
+M_r = M.r
--- startup/40-maia.py
+++ startup/40-maia.py
@@ -1,6 +1,6 @@
 from nslsii.detectors.maia import MAIA
 
-maia = MAIA('XFM:MAIA', name='maia')
+maia = MAIA("XFM:MAIA", name="maia")
 
 import numpy as np
 
@@ -115,7 +115,7 @@ def fly_maia(
 
     # Pitch must match what raster driver uses for pitch ...
     x_pitch = abs(xstop - xstart) / (xnum - 1)
-    #y_pitch = abs(ystop - ystart) / (ynum - 1) #rvt
+    # y_pitch = abs(ystop - ystart) / (ynum - 1) #rvt
 
     # TODO compute this based on someting
     spd_x = x_pitch / dwell
@@ -132,7 +132,7 @@ def fly_maia(
     yield from bps.mv(maia.y_pixel_dim_origin_sp.value, ystart)
 
     yield from bps.mv(maia.x_pixel_dim_pitch_sp.value, x_pitch)
-    #yield from bps.mv(maia.y_pixel_dim_pitch_sp.value, y_pitch) #rvt
+    # yield from bps.mv(maia.y_pixel_dim_pitch_sp.value, y_pitch) #rvt
 
     yield from bps.mv(maia.x_pixel_dim_coord_extent_sp.value, xnum)
     yield from bps.mv(maia.y_pixel_dim_coord_extent_sp.value, ynum)
@@ -142,9 +142,7 @@ def fly_maia(
     yield from bps.mv(maia.meta_val_scan_dwell.value, str(dwell))
 
     yield from bps.mv(maia.meta_val_beam_particle_sp.value, "photon")
-    yield from bps.mv(
-        maia.meta_val_beam_energy_sp.value, "{:.2f}".format(20_000)
-        )
+    yield from bps.mv(maia.meta_val_beam_energy_sp.value, "{:.2f}".format(20_000))
     #    yield from bps.mv(maia.maia_scan_info
     # need something to generate a filename here.
     #    yield from bps.mv(maia.blog_group_next_sp,datafile))
